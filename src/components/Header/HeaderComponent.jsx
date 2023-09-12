a45b5db46cec841e0edd75208df73b1fcdf90786
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-09-12 17:52:15 +0600

    added upto header section

---
+++ src/components/Header/HeaderComponent.jsx
@@ -0,0 +1,75 @@
+import React, { useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import "./HeaderComponent.css"
+
+
+
+const HeaderComponent = () => {
+  const [navOn, setNavOn] = useState(false)
+  const navEle = useRef()
+
+
+  function handleSearch(e) {
+    e.preventDefault()
+    const searchValue = e.target.search.value
+    e.target.search.value = ""
+  }
+
+  function handleNav() {
+    navEle.current.classList.toggle("visible")
+    setNavOn(prev => !prev)
+  }
+
+  return (
+    <header>
+      <div className='headerDiv'>
+        <div>
+          <Link to="/" className='gtlogo'>
+            <img src="/staticImages/globalgtIcon.png" alt='global gt icon' />
+          </Link>
+
+          <form className='product-search' onSubmit={handleSearch}>
+            <input type='text' placeholder='product' name='search' />
+            <button type='submit'>
+              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
+            </button>
+          </form>
+        </div>
+
+        <div >
+          <Link to="/cart"> Cart</Link>
+          <Link to="/myaccount"> My Account</Link>
+
+          <button id='mobile-nav' onClick={handleNav}>
+            {navOn ? <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg> : 
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>
+          }
+          </button>
+        </div>
+
+      </div>
+
+      {/* to use in mobile only  */}
+      <form className='mobile-product-search' onSubmit={handleSearch}>
+        <input type='text' placeholder='product' name='search' />
+        <button type='submit'>
+          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
+        </button>
+      </form>
+
+      <nav ref={navEle}>
+        <Link to="pre-owned">PRE-OWNED</Link>
+        <Link to="refubrished">REFUBRISHED</Link>
+        <Link to="whole-sale">WHOLESLE</Link>
+        <Link to="resources">RESOURCES</Link>
+        <Link to="contact-us">CONTACT US</Link>
+        <Link to="offer">OFFER</Link>
+      </nav>
+
+    </header>
+  )
+}
+
+
+export default HeaderComponent
