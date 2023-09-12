663fbebd121fe68d6c608ab64ab84b110f9a6ec9
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-09-12 23:42:44 +0600

    done upto click on an image to select it then update the product

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -1,12 +1,12 @@
-import  { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import "./AddProduct.css"
 
-// import ImagesForCatagory from '@/components/ImagesForCatagory'
 import ProductForm from '../ProductForm/ProductForm'
 import axiosInstance from '../../utilities/axiosInstance'
 
 import { styled } from 'styled-components'
+import ImageToSelect from '../ImageToSelect/ImageToSelect'
 
 const StyledDiv = styled.div`
   margin: 1rem;
@@ -15,46 +15,51 @@ const StyledDiv = styled.div`
     text-align: center;
     font-size: 1.5rem;
   }
+`
 
+const ImagesHolder = styled.div`
+  display: flex;
+  justify-content: center;
 `
 
 const AddProduct = () => {
     const [allCatagories, setCatagories] = useState([])
-    const [product, setProduct] = useState({parentCatagory:"", image:"", description:"", storage:"", color:"", price:"", discountPrice:"", originalPrice:"", reviewScore:"", peopleReviewed:"", condition:"", color:{name: "", value: ""}})
-    
+    const [product, setProduct] = useState({ parentCatagory: "", image: "", description: "", storage: "", color: "", price: "", discountPrice: "", originalPrice: "", reviewScore: "", peopleReviewed: "", condition: "", color: { name: "", value: "" } })
+
     const [images, setImages] = useState([])
     const [selectedImage, setSelectedImage] = useState()
-  
+
     console.log(images)
-  
+
     useEffect(() => {
-      axiosInstance.get("catagory")
-        .then(result => setCatagories(result.data))
-        .catch(error => console.log(error))
+        axiosInstance.get("catagory")
+            .then(result => setCatagories(result.data))
+            .catch(error => console.log(error))
     }, [])
-  
-    function handleSubmit (e) {
-      e.preventDefault()
-      
-      axiosInstance.post("addsingleproduct", product )
-      .then(res => setProduct({parentCatagory:"", description:"", storage:"", color:"", price:"", originalPrice:"", reviewScore:"", peopleReviewed:"", condition:"", color:{name: "", value: ""}}))
-      .catch(error => alert("error happened !!"))
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        product.image = selectedImage
+        
+        axiosInstance.post("addsingleproduct", product)
+            .then(res => setProduct({ parentCatagory: "", description: "", storage: "", color: "", price: "", originalPrice: "", reviewScore: "", peopleReviewed: "", condition: "", color: { name: "", value: "" } }))
+            .catch(error => alert("error happened !!"))
     }
-  
+
     return (
-      <StyledDiv>
-        <h3>Add a product</h3>
-  
-        {images.map((image, ind) => {
-          return <div key={ind}>
-            <img src={image?.url} alt='product image'/>
-          </div>
-        })}
-  
-        <ProductForm setImages={setImages} allCatagories={allCatagories}  handleSubmit={handleSubmit} product={product} setProduct={setProduct}/>
-  
-      </StyledDiv >
+        <StyledDiv>
+            <h3>Add a product</h3>
+
+            <ImagesHolder>
+                {images.map((image, ind) => {
+                    return <ImageToSelect key={ind} setSelectedImage={setSelectedImage} selectedImage={selectedImage} ImgUrl={image?.url}></ImageToSelect>
+                })}
+            </ImagesHolder>
+
+            <ProductForm setImages={setImages} allCatagories={allCatagories} handleSubmit={handleSubmit} product={product} setProduct={setProduct} />
+
+        </StyledDiv >
     )
-  }
+}
 
 export default AddProduct;
