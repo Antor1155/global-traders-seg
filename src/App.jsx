4ff6c9ba715b6fcfa3ce16bfd639062e9cffcf4c
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-09-23 01:40:25 +0600

    cart item added to local storage, so it will stay

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import { Outlet } from 'react-router-dom'
 import HeaderComponent from './components/Header/HeaderComponent'
 import MyFooter from './components/Footer/MyFooter'
 
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 export const CartContext = createContext([])
 
 function App() {
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState(() => {
+    const savedCart = localStorage.getItem('cart');
+    return savedCart ? JSON.parse(savedCart) : [];
+  });
+
+  useEffect(()=>{
+    localStorage.setItem("cart", JSON.stringify(cart))
+  },[cart])
+
 
   return (
     <>
