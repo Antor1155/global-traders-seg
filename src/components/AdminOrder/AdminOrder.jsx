3c31130421b8fe261b3d6f7b72a755d72a62e575
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-10-17 03:15:25 +0600

    admin order page to show all order based on different filters and basic styling done

--- src/components/AdminOrder/AdminOrder.jsx
+++ src/components/AdminOrder/AdminOrder.jsx
@@ -7,31 +7,48 @@ import SingleAdminOrder from "./SingleAdminOrder";
 const AdminOrder = () => {
     const [orders, setOrders] = useState([])
     const [orderStatus, setOrderStatus] = useState("Processing")
+    const [email, setEmail] = useState("")
+    const [orderId, setOrderId] = useState("")
 
     useEffect(() => {
         axiosInstance.get(`admin-orders/${orderStatus}`)
             .then(res => setOrders(res.data))
             .catch(error => console.log(error))
+        setEmail("")
+        setOrderId("")
     }, [orderStatus])
 
 
     return (
         <main className="admin-order">
-            <div className="statusButtons">
-                <button className={orderStatus==="Processing" ? "active" : {}} onClick={()=>setOrderStatus("Processing")}>Paid</button>
+            <div className="stausFilter">
+                <div className="statusButtons">
+                    <button className={orderStatus === "Processing" ? "active" : {}} onClick={() => setOrderStatus("Processing")}>Paid</button>
 
-                <button className={orderStatus==="Shipped" ? "active" : {}} onClick={()=>setOrderStatus("Shipped")}>Shipped</button>
+                    <button className={orderStatus === "Shipped" ? "active" : {}} onClick={() => setOrderStatus("Shipped")}>Shipped</button>
 
-                <button className={orderStatus==="Delivered" ? "active" : {}} onClick={()=>setOrderStatus("Delivered")}>Delivered</button>
+                    <button className={orderStatus === "Delivered" ? "active" : {}} onClick={() => setOrderStatus("Delivered")}>Delivered</button>
 
-                <button className={orderStatus==="Returned" ? "active" : {}} onClick={()=>setOrderStatus("Returned")}>Returned</button>
+                    <button className={orderStatus === "Returned" ? "active" : {}} onClick={() => setOrderStatus("Returned")}>Returned</button>
 
-                <button className={orderStatus==="Refunded" ? "active" : {}} onClick={()=>setOrderStatus("Refunded")}>Refunded</button>
+                    <button className={orderStatus === "Refunded" ? "active" : {}} onClick={() => setOrderStatus("Refunded")}>Refunded</button>
 
-                <button className={orderStatus==="payment failed" ? "active" : {}} onClick={()=>setOrderStatus("payment failed")}>Payment failed</button>
-            </div>
+                    <button className={orderStatus === "payment failed" ? "active" : {}} onClick={() => setOrderStatus("payment failed")}>Payment failed</button>
+                </div>
 
-            <div>
+                <div className="filterOptions">
+                    <div className="singleFilter">
+                        <input type="text" placeholder="Order Id" value={orderId} onChange={(e)=> setOrderId(e.target.value)} />
+
+                        <button onClick={()=>setOrderStatus(`byOrderId:${orderId}`)}>Search by Id</button>
+                    </div>
+
+                    <div className="singleFilter">
+                        <input type="email" placeholder="Email" value={email} onChange={(e)=> setEmail(e.target.value)} />
+
+                        <button onClick={()=>setOrderStatus(`byEmail:${email}`)}>Search by Email</button>
+                    </div>
+                </div>
 
             </div>
 
