1a43d5fce7da8ab76226273843acea1e8dfe09a9
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-10-28 01:50:53 +0600

    firebase installed and blank login page added

---
+++ src/utilities/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFoGhOr2l4aRV_wP-1TLt3HymKtTW_Qy0",
+  authDomain: "global-traders-usa-84059.firebaseapp.com",
+  projectId: "global-traders-usa-84059",
+  storageBucket: "global-traders-usa-84059.appspot.com",
+  messagingSenderId: "1069022321540",
+  appId: "1:1069022321540:web:419c5d35f4cdd4c64f5bd1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
