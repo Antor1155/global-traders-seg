08023428fb0ae45404c5db034c05eb7f362f0ca0
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-11-02 22:41:52 +0600

    added single customer order.jsx page and single custommer order .css page to show orders to the cusstomers

--- src/components/MyAccount/MyAccount.jsx
+++ src/components/MyAccount/MyAccount.jsx
@@ -1,26 +1,40 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { userContext } from "../../utilities/UserContextProvider";
 import { useNavigate } from "react-router-dom";
+import "./MyAccount.css"
+import axiosInstance from "../../utilities/axiosInstance";
+import SingleCustomerOrder from "./SingleCustomerOrder";
 
 const MyAccount = () => {
-    const {user, loading, logOut} = useContext(userContext)
-    const navigate =  useNavigate()
+    const { user, loading, logOut } = useContext(userContext)
+    const navigate = useNavigate()
 
-    const handleSingOut = ()=>{
-        logOut().then(()=>{
+    const handleSingOut = () => {
+        logOut().then(() => {
             navigate("/")
         }
         ).catch(error => console.log(error))
     }
 
+    const [orders, setOrders] = useState([])
+
+    useEffect(() => {
+        axiosInstance.get(`client-orders/${user?.email}`)
+            .then(res => setOrders(res.data))
+            .catch(error => console.log(error))
+    }, [])
+
+
     return (
-        <div>
-            
-            <h1>This is my account page</h1>
+        <div className="myAccount">
 
+            <div id="accountDetails">
                 <h2>Email: {user?.email}</h2>
-
                 <button onClick={handleSingOut}>Sign Out</button>
+            </div>
+
+            {orders.map(order => <SingleCustomerOrder key={order._id} order={order}>
+            </SingleCustomerOrder>)}
 
         </div>
     );
