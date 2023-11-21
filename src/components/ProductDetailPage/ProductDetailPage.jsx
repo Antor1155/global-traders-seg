3be5bf30045c36e996937b242bcc66d382607cfc
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-11-22 02:17:45 +0600

    productDetailPage.jsx and css changed, most popular and lowest price filter added, onFilter button click, from useEffect to function call transformed

--- src/components/ProductDetailPage/ProductDetailPage.jsx
+++ src/components/ProductDetailPage/ProductDetailPage.jsx
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import axiosInstance from '../../utilities/axiosInstance';
-import "./ProductDetailPage.css"
+import "./ProductDetailPage.css";
+
+import eyes from "../../assets/eyes.svg";
+import stars from "../../assets/stars.svg"
 
 const ProductDetailPage = () => {
     const { parentId, productId } = useParams()
     const [allProducts, setAllProducts] = useState([])
 
     const [product, setProduct] = useState()
+
+    const [lowestPrice, setLowestPrice] = useState()
+    const [mostPopular, setMostPopular] = useState()
 
     const [selectedColor, setSelectedColor] = useState()
     const [selectedStorage, setSelectedStorage] = useState()
@@ -16,6 +22,12 @@ const ProductDetailPage = () => {
     const [availableConditions, setAvailableConditions] = useState([])
     const [availableColors, setAvailableColors] = useState([])
     const [availableStorages, setAvailableStorages] = useState([])
+
+    // style selected button 
+    const selectedButton = {
+        fontWeight: "normal",
+        backgroundColor: "#259ff620"
+    } 
 
     useEffect(() => {
         axiosInstance.get(`/allSameParentProducts/${parentId}`)
@@ -30,18 +42,26 @@ const ProductDetailPage = () => {
                 setSelectedStorage(selectedProduct?.storage)
                 setSelectedCondition(selectedProduct?.condition)
 
+                // Use reduce to find the object with the lowest and heighest value
+                const minObject = products.reduce((min, current) => (current?.price < min?.price ? current : min), products[0]);
+                setLowestPrice(minObject)
+
+                const maxObject = products.reduce((min, current) => (current?.price > min?.price ? current : min), products[0]);
+                setMostPopular(maxObject)
+
+
                 const colors = new Set()
                 const conditions = new Set()
                 const storages = new Set()
 
                 products.forEach(p => {
-                    colors.add(p?.color.name + " " + p?.color.value)
+                    colors.add(p?.color.name + ":" + p?.color.value)
                     conditions.add(p?.condition)
                     storages.add(p?.storage)
                 })
 
                 const finalColors = Array.from(colors).map(str => {
-                    const [name, value] = str.split(" ")
+                    const [name, value] = str.split(":")
                     return { name, value }
                 })
 
@@ -55,14 +75,49 @@ const ProductDetailPage = () => {
             })
     }, [])
 
-    // this useEffect to find proudct when select new option 
-    useEffect(() => {
-        // const newProduct = allProducts.find((p) => p.)
-    }, [selectedColor, selectedCondition, selectedStorage])
+    // this function to find proudct when select new option 
+    const handleFilterButtonClick = (option, value) =>{
+        let color = selectedColor
+        let condition = selectedCondition
+        let storage = selectedStorage
 
-    console.log(availableColors, availableConditions, availableStorages)
-    console.log(product)
-    console.log(selectedColor, selectedCondition, selectedStorage)
+        if(option == "condition"){
+            setSelectedCondition(value);
+            condition = value
+        }
+        else if(option == "color"){   
+            setSelectedColor(value);
+            color = value
+        }
+        else if(option == "storage"){
+            setSelectedStorage(value)
+            storage = value
+        }
+
+        const newProduct = allProducts.find((p) => {
+            return (p?.color.name == color?.name) && (p?.condition == condition) && (p?.storage == storage)
+        })
+
+        setProduct(newProduct)
+    }
+
+    const handleClickLowestPrice = () => {
+        setProduct(lowestPrice)
+        setSelectedColor(lowestPrice?.color)
+        setSelectedStorage(lowestPrice?.storage)
+        setSelectedCondition(lowestPrice?.condition)
+    }
+
+    const handleClickMostPopular = () =>{
+        setProduct(mostPopular)
+        setSelectedColor(mostPopular?.color)
+        setSelectedStorage(mostPopular?.storage)
+        setSelectedCondition(mostPopular?.condition)
+    }
+
+    // console.log(availableColors, availableConditions, availableStorages)
+    // console.log(product)
+    // console.log(selectedColor, selectedCondition, selectedStorage)
 
     return (
         <section>
@@ -70,6 +125,7 @@ const ProductDetailPage = () => {
                 <div className='productImg'>
                     <img src={product?.image} />
                 </div>
+
                 <div className='options'>
                     <div className='name-price'>
                         <h1>{product?.productName} - {product?.storage} -
@@ -81,10 +137,37 @@ const ProductDetailPage = () => {
                     </div>
 
                     <div className='filter-button-container'>
+                        <p>Best picks</p>
+                        <div className='filter-buttons best-picks'>
+                            <button 
+                            onClick={handleClickLowestPrice}
+                            style={product == lowestPrice ? selectedButton : {}}
+                            >
+                                <img src={eyes}></img>
+                                Lowest price <br></br>
+                                ${lowestPrice?.price}
+                            </button>
+
+                            <button 
+                            onClick={handleClickMostPopular}
+                            style={product == mostPopular ? selectedButton : {}}
+                            >
+                                <img src={stars}></img>
+                                Most popular <br></br>
+                                ${mostPopular?.price}
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className='filter-button-container'>
                         <p>Condition</p>
                         <div className='filter-buttons'>
                             {availableConditions.map(condition => (
-                                <button>
+                                <button
+                                    key={condition}
+                                    style={selectedCondition === condition ? selectedButton: {}}
+                                    onClick={() => {handleFilterButtonClick("condition", condition) }}
+                                >
                                     {condition}
                                 </button>
                             ))}
@@ -95,7 +178,11 @@ const ProductDetailPage = () => {
                         <p>Storage (GB)</p>
                         <div className='filter-buttons'>
                             {availableStorages.map(storage => (
-                                <button>
+                                <button
+                                    key={storage}
+                                    style={selectedStorage === storage ? selectedButton : {}}
+                                    onClick={() => {handleFilterButtonClick("storage", storage)}}
+                                >
                                     {storage}
                                 </button>
                             ))}
@@ -106,8 +193,13 @@ const ProductDetailPage = () => {
                         <p>Color</p>
                         <div className='filter-buttons'>
                             {availableColors.map(color => (
-                                <button className='color'>
-                                    <div style={{backgroundColor: color?.value}}></div>
+                                <button
+                                    className='color'
+                                    key={color}
+                                    style={selectedColor?.name === color?.name ? selectedButton : {}}
+                                    onClick={() => {handleFilterButtonClick("color", color)}}
+                                >
+                                    <div style={{ backgroundColor: color?.value }}></div>
                                     {color.name}
                                 </button>
                             ))}
