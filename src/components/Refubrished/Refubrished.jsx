3203731eb042f8bbd11ff969c0815336d12cd969
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-11-30 04:02:37 +0600

    refubrished page with text place holder done, will be later updated

--- src/components/Refubrished/Refubrished.jsx
+++ src/components/Refubrished/Refubrished.jsx
@@ -1,9 +1,13 @@
+import "./Refubrished.css"
 
 const Refubrished = () => {
     return (
-        <div>
-            this is refubrished page
-        </div>
+        <section className="refurbished">
+            <div>
+                <h3>Special products with special price </h3>
+                <h4>Cooming soon...</h4>
+            </div>
+        </section>
     );
 };
 
