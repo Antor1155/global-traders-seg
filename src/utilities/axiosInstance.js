99f23a08c3f46bcf4e4276e0f4dda1925cff7041
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-12-01 05:56:36 +0600

    axisoInstance.js of utilities have baseURL as firebase functions url now

--- src/utilities/axiosInstance.js
+++ src/utilities/axiosInstance.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// const axiosInstance = axios.create({
+//     baseURL:"http://localhost:5000/"
+// })
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:5000/"
+    baseURL:"https://us-central1-global-traders-usa-84059.cloudfunctions.net/app/"
 })
 
 export default axiosInstance
