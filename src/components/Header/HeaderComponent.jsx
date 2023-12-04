28906bb8c75c9ea3c7fc891424ad588e7c566ff9
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-12-05 03:49:20 +0600

    headerComponent.jsx and css icon for cart and account added

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -4,19 +4,22 @@ import { Link } from 'react-router-dom'
 import "./HeaderComponent.css"
 import { CartContext } from '../../App'
 
+import PersonIcon from '@mui/icons-material/Person';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
 
 
 const HeaderComponent = () => {
   const [navOn, setNavOn] = useState(false)
   const navEle = useRef()
 
-  const {cart} = useContext(CartContext)
+  const { cart } = useContext(CartContext)
 
   function handleSearch(e) {
     e.preventDefault()
     const searchValue = e.target.search.value
     e.target.search.value = ""
-    
+
     console.log(searchValue)
   }
 
@@ -44,15 +47,22 @@ const HeaderComponent = () => {
         </div>
 
         <div className='cart-and-account'>
-          {cart?.length ? <span className='cart-amount'>{cart?.length}</span>: ""}
+          {cart?.length ? <span className='cart-amount'>{cart?.length}</span> : ""}
 
-          <Link to="/cart"> Cart</Link>
-          <Link to="/myaccount"> My Account</Link>
+          <Link to="/cart">
+            <span className='nav-icon-mobile'><PersonIcon></PersonIcon></span>
+            <span className='mobile-hide'>Cart</span>
+          </Link>
+
+          <Link to="/myaccount">
+            <span className='nav-icon-mobile'><ShoppingCartIcon></ShoppingCartIcon></span>
+            <span className='mobile-hide'>My Account</span>
+          </Link>
 
           <button id='mobile-nav' onClick={handleNav}>
-            {navOn ? <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg> : 
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>
-          }
+            {navOn ? <svg xmlns="http://www.w3.org/2000/svg"  height="1em" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" /></svg> :
+              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>
+            }
           </button>
         </div>
 
