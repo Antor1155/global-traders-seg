82a6753abf4e2af7e80bc23263c27e04802a3965
Md Antor <md.antor1155@gmail.com>
2024-03-29 18:15:30 +0600

    trade difference before after section design for mobile and desktop setup done next add proper content and images

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,6 +8,7 @@ import Comments from "../Comments/Comments";
 import { useEffect, useRef, useState } from "react";
 import axiosInstance from "../../utilities/axiosInstance";
 import ScrollToTop from "../../utilities/ScrollToTop";
+import TradeDifference from "./TradeDifference";
 
 const Home = () => {
     const [nProducts, setNproducts] = useState([])
@@ -33,100 +34,103 @@ const Home = () => {
 
 
     return (
-        <main>
-            <ScrollToTop></ScrollToTop>
-            <BootstrapCarousel></BootstrapCarousel>
+        <>
+            <main>
+                <ScrollToTop></ScrollToTop>
+                <BootstrapCarousel></BootstrapCarousel>
 
-            <div className='logos'>
-                <div className="logos-container">
-                    <div>
-                        <img src="/logos/return.png" alt='return' />
-                        <span> 1 month Warrenty</span>
+                <div className='logos'>
+                    <div className="logos-container">
+                        <div>
+                            <img src="/logos/return.png" alt='return' />
+                            <span> 1 month Warrenty</span>
+                        </div>
+
+                        <div>
+                            <img src="/logos/giftcard.png" alt='giftcard' />
+                            <span> GT Gift Card</span>
+                        </div>
+
+                        <div>
+                            <img src="/logos/fastdelivery.png" alt='fastdelivery' />
+                            <span> Fast Delivery</span>
+                        </div>
+
+
+                        <div>
+                            <img src="/logos/return.png" alt='return' />
+                            <span> 30 Days Return</span>
+                        </div>
+
+                        <div>
+                            <img src="/logos/trustedseller.png" alt='trustedseller' />
+                            <span> Trusted Seller</span>
+                        </div>
+
+                        <div>
+                            <img src="/logos/klarna.png" alt='klarna' />
+                            <span> Payment Plan</span>
+                        </div>
+
+
+                        {/* show on mobile screen only  */}
+                        <div className="mobile-view">
+                            <img src="/logos/return.png" alt='return' />
+                            <span> 1 month Warrenty</span>
+                        </div>
+
+                        <div className="mobile-view">
+                            <img src="/logos/giftcard.png" alt='giftcard' />
+                            <span> GT Gift Card</span>
+                        </div>
+
+                        <div className="mobile-view">
+                            <img src="/logos/fastdelivery.png" alt='fastdelivery' />
+                            <span> Fast Delivery</span>
+                        </div>
+
                     </div>
+                </div>
 
-                    <div>
-                        <img src="/logos/giftcard.png" alt='giftcard' />
-                        <span> GT Gift Card</span>
-                    </div>
-
-                    <div>
-                        <img src="/logos/fastdelivery.png" alt='fastdelivery' />
-                        <span> Fast Delivery</span>
-                    </div>
-
-                    
-                    <div>
-                        <img src="/logos/return.png" alt='return' />
-                        <span> 30 Days Return</span>
-                    </div>
-
-                    <div>
-                        <img src="/logos/trustedseller.png" alt='trustedseller' />
-                        <span> Trusted Seller</span>
-                    </div>
-
-                    <div>
-                        <img src="/logos/klarna.png" alt='klarna' />
-                        <span> Payment Plan</span>
-                    </div>
+                <div className='pre-and-refub'>
+                    <Link to="preowned"> PRE-OWNED</Link>
+                    <Link to="wholesale"> WHOLESALE</Link>
+                </div>
 
 
-                    {/* show on mobile screen only  */}
-                    <div className="mobile-view">
-                        <img src="/logos/return.png" alt='return' />
-                        <span> 1 month Warrenty</span>
-                    </div>
-
-                    <div className="mobile-view">
-                        <img src="/logos/giftcard.png" alt='giftcard' />
-                        <span> GT Gift Card</span>
-                    </div>
-
-                    <div className="mobile-view">
-                        <img src="/logos/fastdelivery.png" alt='fastdelivery' />
-                        <span> Fast Delivery</span>
-                    </div>
+                <div className='products'>
+                    {nProducts && nProducts.slice(0, 4).map(product => {
+                        return <SingleProduct key={product._id} product={product}></SingleProduct>
+                    })}
 
                 </div>
-            </div>
 
-            <div className='pre-and-refub'>
-                <Link to="preowned"> PRE-OWNED</Link>
-                <Link to="wholesale"> WHOLESALE</Link>
-            </div>
+                <Link to="/preowned" className='see-more'> See More</Link>
 
+                <div className='adds'>
+                    <img src='/adds/add1.png' alt='product add' />
+                    <img src='/adds/add2.png' alt='product add' />
+                    <img src='/adds/add3.png' alt='product add' />
+                    <img src='/adds/add4.png' alt='product add' />
+                </div>
 
-            <div className='products'>
-                {nProducts && nProducts.slice(0, 4).map(product => {
-                    return <SingleProduct key={product._id} product={product}></SingleProduct>
-                })}
+                <Link to="/preowned">
+                    <img className='sub-banner' src="/staticImages/second-banner.png" alt="second banner" />
+                </Link>
 
-            </div>
+                <h3 className="text-top-deals">TOP <span>DEALS</span> </h3>
 
-            <Link to="/preowned" className='see-more'> See More</Link>
+                <div className='top-delas-products'>
+                    {nProducts && nProducts.map(product => {
+                        return <TopDeals key={product._id} product={product}></TopDeals>
+                    })}
+                </div>
+            </main>
 
-            <div className='adds'>
-                <img src='/adds/add1.png' alt='product add' />
-                <img src='/adds/add2.png' alt='product add' />
-                <img src='/adds/add3.png' alt='product add' />
-                <img src='/adds/add4.png' alt='product add' />
-            </div>
-
-            <Link to="/preowned">
-                <img className='sub-banner' src="/staticImages/second-banner.png" alt="second banner" />
-            </Link>
-
-            <h3 className="text-top-deals">TOP <span>DEALS</span> </h3>
-
-            <div className='top-delas-products'>
-                {nProducts && nProducts.map(product => {
-                    return <TopDeals key={product._id} product={product}></TopDeals>
-                })}
-            </div>
+            <TradeDifference />
 
             <Comments></Comments>
-
-        </main>
+        </>
     );
 };
 
---
+++ src/components/Home/TradeDifference.jsx
@@ -0,0 +1,85 @@
+import "./TradeDifference.css"
+
+const TradeDifference = () => {
+    return (
+        <section id="tradeDifference">
+
+            <div id="experience">
+                <div className="container-td">
+                    <h3>EXPERIENCE THE TRADE DIFFERENCE</h3>
+                    <p>We believe buying used phones should be just as exciting as buying new.
+                        Here’s how Global Traders is changing the game.
+                    </p>
+                </div>
+            </div>
+
+
+            <div id="before-section">
+                <h4>BEFORE</h4>
+
+                <div className="container-td">
+                    <div>
+                        <img src="/logos/beforeIcons1.svg" alt="before service icons" />
+                        <p>HAGGLING WAS MANDATORY</p>
+                        <p>You had to negotiate a good deal with a dealer or private seller. To complicate things, you never knew if you were dealing with someone trustworthy.
+                        </p>
+                    </div>
+                    <div>
+                        <img src="/logos/beforeIcons1.svg" alt="before service icons" />
+                        <p>HAGGLING WAS MANDATORY</p>
+                        <p>You had to negotiate a good deal with a dealer or private seller. To complicate things, you never knew if you were dealing with someone trustworthy.
+                        </p>
+                    </div>
+                    <div>
+                        <img src="/logos/beforeIcons1.svg" alt="before service icons" />
+                        <p>HAGGLING WAS MANDATORY</p>
+                        <p>You had to negotiate a good deal with a dealer or private seller. To complicate things, you never knew if you were dealing with someone trustworthy.
+                        </p>
+                    </div>
+                    <div>
+                        <img src="/logos/beforeIcons1.svg" alt="before service icons" />
+                        <p>HAGGLING WAS MANDATORY</p>
+                        <p>You had to negotiate a good deal with a dealer or private seller. To complicate things, you never knew if you were dealing with someone trustworthy.
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+
+            <div id="after-section">
+                <h4>AFTER</h4>
+
+                <div className="container-td">
+                    <div>
+                        <img src="/logos/beforeIcons1.svg" alt="before service icons" />
+                        <p>HAGGLING WAS MANDATORY</p>
+                        <p>You had to negotiate a good deal with a dealer or private seller. To complicate things, you never knew if you were dealing with someone trustworthy.
+                        </p>
+                    </div>
+                    <div>
+                        <img src="/logos/beforeIcons1.svg" alt="before service icons" />
+                        <p>HAGGLING WAS MANDATORY</p>
+                        <p>You had to negotiate a good deal with a dealer or private seller. To complicate things, you never knew if you were dealing with someone trustworthy.
+                        </p>
+                    </div>
+                    <div>
+                        <img src="/logos/beforeIcons1.svg" alt="before service icons" />
+                        <p>HAGGLING WAS MANDATORY</p>
+                        <p>You had to negotiate a good deal with a dealer or private seller. To complicate things, you never knew if you were dealing with someone trustworthy.
+                        </p>
+                    </div>
+                    <div>
+                        <img src="/logos/beforeIcons1.svg" alt="before service icons" />
+                        <p>HAGGLING WAS MANDATORY</p>
+                        <p>You had to negotiate a good deal with a dealer or private seller. To complicate things, you never knew if you were dealing with someone trustworthy.
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+    );
+};
+
+export default TradeDifference;
