9f11f527f19082d8c2d47322cefab875f17275d7
Md Antor <md.antor1155@gmail.com>
2024-03-30 13:54:22 +0600

    tradeDifference.jsx all info and image setup, next make style of the gird divs

--- src/components/Home/TradeDifference.jsx
+++ src/components/Home/TradeDifference.jsx
@@ -25,21 +25,31 @@ const TradeDifference = () => {
                         </p>
                     </div>
                     <div>
-                        <img src="/logos/beforeIcons1.svg" alt="before service icons" />
-                        <p>HAGGLING WAS MANDATORY</p>
-                        <p>You had to negotiate a good deal with a dealer or private seller. To complicate things, you never knew if you were dealing with someone trustworthy.
+                        <img src="/logos/beforeIcons2.svg" alt="before service icons" />
+                        <p>HIDDEN
+                            PROBLEMS</p>
+                        <p>You thought you scored a great phone,
+                            only to find out later that the phone has
+                            functional issues that appeared weeks later.
                         </p>
                     </div>
                     <div>
-                        <img src="/logos/beforeIcons1.svg" alt="before service icons" />
-                        <p>HAGGLING WAS MANDATORY</p>
-                        <p>You had to negotiate a good deal with a dealer or private seller. To complicate things, you never knew if you were dealing with someone trustworthy.
+                        <img src="/logos/beforeIcons3.svg" alt="before service icons" />
+                        <p>BUYER’S REMORSE?
+                            YOU’RE STUCK</p>
+                        <p>You thought you scored a great phone,
+                            only to find out later that the phone has
+                            functional issues that appeared weeks later.
                         </p>
                     </div>
                     <div>
-                        <img src="/logos/beforeIcons1.svg" alt="before service icons" />
-                        <p>HAGGLING WAS MANDATORY</p>
-                        <p>You had to negotiate a good deal with a dealer or private seller. To complicate things, you never knew if you were dealing with someone trustworthy.
+                        <img src="/logos/beforeIcons4.svg" alt="before service icons" />
+                        <p>ONLY NEW PHONES
+                            WERE PROTECTED</p>
+                        <p>Device protections generally cover new phones.
+                            This leaves second hand owners vulnerable when
+                            accidents happen.
+
                         </p>
                     </div>
 
@@ -51,27 +61,42 @@ const TradeDifference = () => {
 
                 <div className="container-td">
                     <div>
-                        <img src="/logos/beforeIcons1.svg" alt="before service icons" />
-                        <p>HAGGLING WAS MANDATORY</p>
-                        <p>You had to negotiate a good deal with a dealer or private seller. To complicate things, you never knew if you were dealing with someone trustworthy.
+                        <img src="/logos/afterIcons1.svg" alt="before service icons" />
+                        <p>PHONES PRICED A
+                            FAIR MARKET VALUE</p>
+                        <p>You won’t find scammers here. This means you get
+                            what you pay for, at a fair deal, with no surprises.
                         </p>
                     </div>
                     <div>
-                        <img src="/logos/beforeIcons1.svg" alt="before service icons" />
-                        <p>HAGGLING WAS MANDATORY</p>
-                        <p>You had to negotiate a good deal with a dealer or private seller. To complicate things, you never knew if you were dealing with someone trustworthy.
+                        <img src="/logos/afterIcons2.svg" alt="before service icons" />
+                        <p>MULTI-POINT
+                            INSPECTION</p>
+                        <p>Every phone we sell passes our rigorous inspection
+                            process and includes a full phone history report.
+                            We disclose everything about the phone so
+                            there are no surprises.
                         </p>
                     </div>
                     <div>
-                        <img src="/logos/beforeIcons1.svg" alt="before service icons" />
-                        <p>HAGGLING WAS MANDATORY</p>
-                        <p>You had to negotiate a good deal with a dealer or private seller. To complicate things, you never knew if you were dealing with someone trustworthy.
+                        <img src="/logos/afterIcons3.svg" alt="before service icons" />
+                        <p>30 DAY
+                            TRIAL</p>
+                        <p>Use the phone for up to two weeks.
+                            Get familiar with your new phone and put
+                            it through its paces as you normally would.
+                            If you’re not completely satisfied, we’ll take
+                            it back and refund you the full amount.
                         </p>
                     </div>
                     <div>
-                        <img src="/logos/beforeIcons1.svg" alt="before service icons" />
-                        <p>HAGGLING WAS MANDATORY</p>
-                        <p>You had to negotiate a good deal with a dealer or private seller. To complicate things, you never knew if you were dealing with someone trustworthy.
+                        <img src="/logos/afterIcons4.svg" alt="before service icons" />
+                        <p>GLOBAL TRADERS
+                            PROTECT</p>
+                        <p>Extended hardware warranty, protection against
+                            accidental damage, and technical support
+                            can now be obtained for an additional $7/month.
+                            No long term commitments. Cancel anytime.
                         </p>
                     </div>
 
