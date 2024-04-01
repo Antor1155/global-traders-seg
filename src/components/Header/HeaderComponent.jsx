7c495c3d0e8ad3a2f61ce150077af77c996325ca
Md Antor <md.antor1155@gmail.com>
2024-04-01 17:47:47 +0600

    active link is highlighted on navbar

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -1,5 +1,5 @@
 import { useContext, useRef, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 
 import "./HeaderComponent.css"
 import { CartContext } from '../../App'
@@ -43,7 +43,7 @@ const HeaderComponent = () => {
           </Link>
 
           <form className='product-search' onSubmit={handleSearch}>
-            <input type='text' placeholder='Search for a Phone' name='search' required/>
+            <input type='text' placeholder='Search for a Phone' name='search' required />
             <button type='submit'>
               <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
             </button>
@@ -76,19 +76,31 @@ const HeaderComponent = () => {
 
       {/* to use in mobile only  */}
       <form className='mobile-product-search' onSubmit={handleSearch}>
-        <input type='text' placeholder='Search for a Phone' name='search' required/>
+        <input type='text' placeholder='Search for a Phone' name='search' required />
         <button type='submit'>
           <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
         </button>
       </form>
 
       <nav ref={navEle}>
-        <Link onClick={handleNav} to="preowned">PRE-OWNED</Link>
-        <Link onClick={handleNav} to="refubrished">REFURBISHED</Link>
-        <Link onClick={handleNav} to="wholesale">WHOLESALE</Link>
-        <Link onClick={handleNav} to="resources">RESOURCES</Link>
-        <Link onClick={handleNav} to="contactus">CONTACT US</Link>
-        <Link onClick={handleNav} to="offer">OFFER</Link>
+        <NavLink className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        } onClick={handleNav} to="preowned">PRE-OWNED</ NavLink>
+        <NavLink className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        } onClick={handleNav} to="refubrished">REFURBISHED</ NavLink>
+        <NavLink className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        } onClick={handleNav} to="wholesale">WHOLESALE</ NavLink>
+        <NavLink className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        } onClick={handleNav} to="resources">RESOURCES</ NavLink>
+        <NavLink className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        } onClick={handleNav} to="contactus">CONTACT US</ NavLink>
+        <NavLink className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        } onClick={handleNav} to="offer">OFFER</ NavLink>
       </nav>
 
     </header>
