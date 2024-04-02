4fe4b0bb637e87e6cfae9c6d998036b82c2b53d8
Md Antor <md.antor1155@gmail.com>
2024-04-02 14:49:27 +0600

    wholesale page have notification on submission form

--- src/components/Wholesale/Wholesale.jsx
+++ src/components/Wholesale/Wholesale.jsx
@@ -1,3 +1,4 @@
+import { toast } from "react-toastify";
 import "./Wholesale.css"
 // import axiosInstance from "../../utilities/axiosInstance";
 
@@ -13,6 +14,7 @@ const Wholesale = () => {
         
         const data = {name, phone, email, company, note}
 
+        toast("Thank you for your query. We will contact you soon.!!")
 
         e.target.reset()
 
