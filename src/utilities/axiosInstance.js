1e623465eb93822fdfecea1df12edb72eafeacd3
Antor1155 <md.antor1155@gmail.com>
2024-04-06 13:48:35 +0600

    axiosInstance.js file updated to gt fibase functions link

--- src/utilities/axiosInstance.js
+++ src/utilities/axiosInstance.js
@@ -5,14 +5,14 @@ import axios from "axios";
 // })
 
 // using cloud functions with firebase
-// const axiosInstance = axios.create({
-//     baseURL:"https://us-central1-global-traders-usa-84059.cloudfunctions.net/app/"
-// })
+const axiosInstance = axios.create({
+    baseURL:"https://us-central1-globaltraders-3f62a.cloudfunctions.net/app/"
+})
 
 
 // using cyclic .sh app 
-const axiosInstance = axios.create({
-    baseURL:"https://easy-erin-bluefish-wear.cyclic.app/"
-})
+// const axiosInstance = axios.create({
+//     baseURL:"https://easy-erin-bluefish-wear.cyclic.app/"
+// })
 
 export default axiosInstance
