79b84769a6c9cb7723ba5875c0d7c201ccc9842f
Antor1155 <md.antor1155@gmail.com>
2024-09-03 03:16:21 +0600

    feat: fetch number from db for current month's sell and update it form admin panel set

--- src/components/AdminHome/AdminHome.jsx
+++ src/components/AdminHome/AdminHome.jsx
@@ -1,112 +1,160 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { userContext } from '../../utilities/UserContextProvider';
-import axiosInstance from '../../utilities/axiosInstance';
-import "./AdminHome.css"
-
+import React, { useContext, useEffect, useState } from "react";
+import { userContext } from "../../utilities/UserContextProvider";
+import axiosInstance from "../../utilities/axiosInstance";
+import "./AdminHome.css";
 
 const AdminHome = () => {
-    const [ordersToday, setOrdersToday] = useState({ amount: 0, money: 0 })
-    const [ordersWeek, setOrdersWeek] = useState({ amount: 0, money: 0 })
-    const [ordersMonth, setOrderMonth] = useState({ amount: 0, money: 0 })
+  const [ordersToday, setOrdersToday] = useState({ amount: 0, money: 0 });
+  const [ordersWeek, setOrdersWeek] = useState({ amount: 0, money: 0 });
+  const [ordersMonth, setOrderMonth] = useState({ amount: 0, money: 0 });
 
-    const { user, loading } = useContext(userContext)
+  const { user, loading } = useContext(userContext);
 
-    useEffect(() => {
-        axiosInstance.get("admin-orders-by-data")
-            .then(res => {
-                const totalOrders = res.data
+  useEffect(() => {
+    axiosInstance
+      .get("admin-orders-by-data")
+      .then((res) => {
+        const totalOrders = res.data;
 
-                let todayAmount = 0
-                let weekAmount = 0
-                let monthAmount = 0
+        let todayAmount = 0;
+        let weekAmount = 0;
+        let monthAmount = 0;
 
-                let todayMoney = 0
-                let weekMoney = 0
-                let monthMoney = 0
+        let todayMoney = 0;
+        let weekMoney = 0;
+        let monthMoney = 0;
 
-                totalOrders.today.forEach(items => {
-                    if (items.paid) {
-                        todayAmount++
-                        items.line_items.forEach(i => {
-                            todayMoney += i.price_data.unit_amount / 100
-                        })
-                    }
-                })
+        totalOrders.today.forEach((items) => {
+          if (items.paid) {
+            todayAmount++;
+            items.line_items.forEach((i) => {
+              todayMoney += i.price_data.unit_amount / 100;
+            });
+          }
+        });
 
-                totalOrders.thisWeek.forEach(items => {
-                    if (items.paid) {
-                        weekAmount++
-                        items.line_items.forEach(i => {
-                            weekMoney += i.price_data.unit_amount / 100
-                        })
-                    }
-                })
+        totalOrders.thisWeek.forEach((items) => {
+          if (items.paid) {
+            weekAmount++;
+            items.line_items.forEach((i) => {
+              weekMoney += i.price_data.unit_amount / 100;
+            });
+          }
+        });
 
-                totalOrders.thisMonth.forEach(items => {
-                    if (items.paid) {
-                        monthAmount++
-                        items.line_items.forEach(i => {
-                            monthMoney += i.price_data.unit_amount / 100
-                        })
-                    }
-                })
+        totalOrders.thisMonth.forEach((items) => {
+          if (items.paid) {
+            monthAmount++;
+            items.line_items.forEach((i) => {
+              monthMoney += i.price_data.unit_amount / 100;
+            });
+          }
+        });
 
-                setOrdersToday({ amount: todayAmount, money: todayMoney })
-                setOrdersWeek({ amount: weekAmount, money: weekMoney })
-                setOrderMonth({ amount: monthAmount, money: monthMoney })
-            })
-            .catch(error => console.log("error in adminHome.jsx", error))
-    }, [])
+        setOrdersToday({ amount: todayAmount, money: todayMoney });
+        setOrdersWeek({ amount: weekAmount, money: weekMoney });
+        setOrderMonth({ amount: monthAmount, money: monthMoney });
+      })
+      .catch((error) => console.log("error in adminHome.jsx", error));
+  }, []);
 
-    return (
-        <section id='adminHome'>
-            {user &&
-                <h3>Welcome: <span>{user.email}</span></h3>
-            }
+  const handleUpdateMonthlySells = async (e) => {
+    e.preventDefault();
+    const amount = e.target.amount.value;
 
-            <div id='stats'>
+    if (!isNaN(amount)) {
+      axiosInstance
+        .post("this-month-sold-items", { name: "gt-sells", amount })
+        .then((res) => {
+          if (res.data === "saved successfully") {
+            alert("UPDATED SUCCESSFULLY");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("failed to update");
+        })
+        .finally(() => {
+          e.target.amount.value = "";
+        });
+    } else {
+      alert("Please enter number");
+    }
+  };
 
-                <div>
-                    <p>TODAY</p>
-                    <p className="big">{ordersToday?.amount}</p>
-                    <small>Total orders today</small>
-                </div>
+  return (
+    <section id="adminHome">
+      {user && (
+        <h3>
+          Welcome: <span>{user.email}</span>
+        </h3>
+      )}
 
-                <div>
-                    <p>THIS WEEK</p>
-                    <p className="big">{ordersWeek?.amount}</p>
-                    <small>Total orders this week</small>
-                </div>
+      <div id="stats">
+        <div>
+          <p>TODAY</p>
+          <p className="big">{ordersToday?.amount}</p>
+          <small>Total orders today</small>
+        </div>
 
-                <div>
-                    <p>THIS MONTH</p>
-                    <p className="big">{ordersMonth?.amount}</p>
-                    <small>Total orders this month</small>
-                </div>
+        <div>
+          <p>THIS WEEK</p>
+          <p className="big">{ordersWeek?.amount}</p>
+          <small>Total orders this week</small>
+        </div>
 
-                {/* prices */}
+        <div>
+          <p>THIS MONTH</p>
+          <p className="big">{ordersMonth?.amount}</p>
+          <small>Total orders this month</small>
+        </div>
 
-                <div>
-                    <p>TODAY</p>
-                    <p className="big">$ {ordersToday?.money}</p>
-                    <small>Total orders amount today</small>
-                </div>
+        {/* prices */}
 
-                <div>
-                    <p>THIS WEEK</p>
-                    <p className="big">$ {ordersWeek?.money}</p>
-                    <small>Total orders amount this week</small>
-                </div>
+        <div>
+          <p>TODAY</p>
+          <p className="big">$ {ordersToday?.money}</p>
+          <small>Total orders amount today</small>
+        </div>
 
-                <div>
-                    <p>THIS MONTH</p>
-                    <p className="big">$ {ordersMonth?.money}</p>
-                    <small>Total orders amount this month</small>
-                </div>
+        <div>
+          <p>THIS WEEK</p>
+          <p className="big">$ {ordersWeek?.money}</p>
+          <small>Total orders amount this week</small>
+        </div>
 
-            </div>
-        </section>
-    );
+        <div>
+          <p>THIS MONTH</p>
+          <p className="big">$ {ordersMonth?.money}</p>
+          <small>Total orders amount this month</small>
+        </div>
+      </div>
+
+      <div style={{ margin: "5rem auto", fontWeight: "bold" }}>
+        <p>Update current month's sell amount to show on website</p>
+        <form
+          style={{ display: "flex", gap: "1rem" }}
+          onSubmit={handleUpdateMonthlySells}
+        >
+          <input type="text" id="amount" name="amount" />
+
+          <button
+            style={{
+              color: "#249EF5",
+              fontSize: "1rem",
+              fontWeight: "bold",
+              border: "1px solid ",
+              borderRadius: "10px",
+              padding: "5px 1rem",
+            }}
+            type="submit"
+          >
+            Update
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default AdminHome;
